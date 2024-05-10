c2fac713714f427e7b4acc22d1edd1bfbf74c321
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-10 10:21:28 +0200

    Add GTE base notebooks and indexes

---
+++ gte-base-en-v1.5/run_one_regular2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name="test_library"
+#SBATCH --time=00:6:00
+#SBATCH --partition=gpu-a100
+#SBATCH --gpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=Education-EEMCS-Courses-CSE3000
+
+module load 2023r1
+module load python
+module load py-pip
+module load openjdk
+module load cuda
+
+
+# Install dependencies
+python -m pip install --user python-terrier==0.10.0 fast-forward-indexes==0.2.0 jupyter ipywidgets transformers typing pathlib
+
+# Run the experiment
+srun python index_one_dataset2.py > prints.txt
+
