8b956b49cae8d35b7c02e61d20c00e7847dbe151
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-10 21:35:27 +0200

    Cells still run to infinity

---
+++ gte-base-en-v1.5/run_only_cpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name="test_library"
+#SBATCH --time=00:6:00
+#SBATCH --partition=compute
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=16G
+#SBATCH --account=Education-EEMCS-Courses-CSE3000
+
+module load 2023r1
+module load python
+module load py-pip
+module load openjdk
+module load cuda
+
+
+# Install dependencies
+python -m pip install --user python-terrier==0.10.0 fast-forward-indexes==0.2.0 jupyter ipywidgets transformers typing pathlib
+
+# Run the experiment
+srun python run_python_cell.py > prints.txt
+
