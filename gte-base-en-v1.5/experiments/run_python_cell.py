70a4d1609a258f36321d9a76cb3aa4b4f58fcad3
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-11 14:11:14 +0200

    experiments notebook has all timed experiments with the working datasets until now

--- gte-base-en-v1.5/experiments/run_python_cell.py
+++ gte-base-en-v1.5/experiments/run_python_cell.py
@@ -1,4 +1,4 @@
-from experiments_helper import split_dev_test, default_complete_test_pipeline, default_complete_test_pipeline_nogrid
+from experiments_helper import split_dev_test, time_fct_print_results, default_complete_test_pipeline
 from pyterrier.measures import RR, nDCG, MAP
 from gte_base_en_encoder import GTEBaseDocumentEncoder
 from memory_profiler import profile
@@ -13,9 +13,6 @@ def runtest():
     eval_metrics = [RR @ 10, nDCG @ 10, MAP @ 100]
 
     q_encoder = GTEBaseDocumentEncoder("Alibaba-NLP/gte-base-en-v1.5")
-    # dataset_name = "cqadupstack/english"
-    # dataset = pt.get_dataset("irds:beir/cqadupstack/english")
-    # topics = dataset.get_topics('text')
 
     dataset_name = "arguana"
     dataset = pt.get_dataset("irds:beir/arguana")
@@ -23,9 +20,8 @@ def runtest():
 
     dev_topics, test_topics = split_dev_test(topics, test_size=0.8)
 
-    x = default_complete_test_pipeline_nogrid(dataset_name, dataset.get_qrels(), dev_topics, test_topics, q_encoder,
-                                              eval_metrics)
-    print(x)
+    time_fct_print_results(default_complete_test_pipeline, dataset_name, dataset.get_qrels(), test_topics, q_encoder,
+                           eval_metrics)
 
 
 @profile
