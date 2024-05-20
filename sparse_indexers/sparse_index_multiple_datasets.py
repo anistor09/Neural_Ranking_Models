b31a6f29476e9de6833b8ba22a686233a7c1ac4e
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-20 10:21:13 +0200

    Add dbpedia sparse indexeres + include sparse indexes in defltblue

--- sparse_indexers/sparse_index_multiple_datasets.py
+++ sparse_indexers/sparse_index_multiple_datasets.py
@@ -1,11 +1,28 @@
 from sparse_index_one_dataset import index_one
 
+datasets = ["nfcorpus", "cqadupstack/english", "arguana", "scidocs", "scifact", "fiqa"]
+lengths = [8, 6, 47, 40, 9, 6]
+prefix_dataset = "irds:beir/"
 
-def index_multiple(prefix_dataset, model_name, datasets, lengths):
+
+def index_multiple(prefix_dataset, datasets, lengths):
     for index, dataset_name in enumerate(datasets):
         try:
-            index_one(prefix_dataset, dataset_name, model_name, lengths[index])
+            index_one(prefix_dataset, dataset_name, lengths[index])
             print(dataset_name + " DONE")
         except Exception as e:
 
             print(f"An error occurred: {e}")
+
+
+def main():
+    try:
+        index_multiple(prefix_dataset, datasets, lengths)
+
+    except Exception as e:
+        # Handles any other exceptions
+        print(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    main()
--- sparse_indexers/sparse_index_one_dataset.py
+++ sparse_indexers/sparse_index_one_dataset.py
@@ -2,23 +2,23 @@ from memory_profiler import profile
 import pyterrier as pt
 
 
-def index_one(prefix_dataset, dataset_name, model_name, max_doc_id_length):
+def index_one(prefix_dataset, dataset_name, max_doc_id_length):
     if not pt.started():
         pt.init(tqdm="notebook")
 
     dataset = pt.get_dataset(prefix_dataset + dataset_name)
 
-    index_path = "../sparse_indexes/sparse_index_" + model_name + "_" + dataset_name
+    index_path = "./sparse_indexes/sparse_index_" + dataset_name
     indexer = pt.IterDictIndexer(index_path, meta={'docno': max_doc_id_length})
     indexer.index(dataset.get_corpus_iter(), fields=["text"])
 
 
-# @profile
+@profile
 def main():
-    dataset_name = "scifact"
-    model_name = "gte-base-en-v1.5"
     prefix_dataset = "irds:beir/"
-    index_one(prefix_dataset, dataset_name, model_name)
+    dataset_name = "dbpedia-entity"
+    max_id_length = 200
+    index_one(prefix_dataset, dataset_name, max_id_length)
 
 
 if __name__ == '__main__':
