df9c83382182b252f6e137e731402c898ea1aa71
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-26 21:32:08 +0200

    Solve gte metrics script bug

--- gte_base_en_v1_5/experiments/ranking_measures_gte_base.py
+++ gte_base_en_v1_5/experiments/ranking_measures_gte_base.py
@@ -6,7 +6,7 @@ from func_timeout import func_timeout
 def main():
     package = "Alibaba-NLP/"
     model_name = "gte-base-en-v1.5"
-    q_encoder = GTEBaseEncoder(model_name + package)
+    q_encoder = GTEBaseEncoder(package + model_name)
     project_directory = "gte_base_en_v1_5"
     func_timeout(5 * 3600 - 120, get_ranking_performance_bge, args=(q_encoder, project_directory, model_name))
 
