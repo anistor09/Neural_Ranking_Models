4c9b8a38eac177a7ddd830292943dfc9c8a32066
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-26 22:09:24 +0200

    Index collections with arctic embed xs

--- snowflake/dense_indexers/dense_index_multiple_datasets.py
+++ snowflake/dense_indexers/dense_index_multiple_datasets.py
@@ -1,19 +1,30 @@
 from snowflake.dense_indexers.dense_index_one_dataset import index_snowflake_m_collection
+from snowflake.dense_indexers.dense_index_one_dataset import index_snowflake_xs_collection
+from func_timeout import func_timeout
 
-datasets = ["quora", "fever"]
-lengths = [6, 221]
+beir_datasets = ["dbpedia-entity", "fever", "fiqa", "hotpotqa", "nfcorpus", "quora"]
+lengths = [200, 221, 6, 8, 8, 6]
 prefix_dataset = "irds:beir/"
 directory = "snowflake"
 
 
 def index_snowflake_m_collections():
-    for index, dataset_name in enumerate(datasets):
+    for index, dataset_name in enumerate(beir_datasets):
         index_snowflake_m_collection(prefix_dataset + dataset_name, lengths[index], directory)
+
+
+def index_snowflake_xs_collections():
+    datasets = [prefix_dataset + dataset for dataset in beir_datasets]
+    datasets.append("irds:msmarco-passage/trec-dl-2019")
+    lengths.append(7)
+
+    for index, dataset_name in enumerate(beir_datasets):
+        index_snowflake_xs_collection(prefix_dataset + dataset_name, lengths[index], directory)
 
 
 def main():
     try:
-        index_snowflake_m_collections()
+        func_timeout(24 * 3600 - 120, index_snowflake_xs_collections)
     except Exception as e:
         # Handles any other exceptions
         print(f"An error occurred: {e}")
--- snowflake/dense_indexers/dense_index_one_dataset.py
+++ snowflake/dense_indexers/dense_index_one_dataset.py
@@ -4,13 +4,13 @@ from general_dense_indexers.dense_index_one_dataset import index_collection
 from encoders.snowflake_arctic_embed_m import SnowFlakeDocumentEncoder, SnowFlakeQueryEncoder
 
 
-def index_snowflake_collection(dataset_name, max_id_length, directory, model_name):
+def index_snowflake_collection(dataset_name, max_id_length, directory, model_name, dim=768):
     q_encoder = SnowFlakeQueryEncoder("Snowflake/" + model_name)
     d_encoder = SnowFlakeDocumentEncoder(
         "Snowflake/" + model_name,
         device="cuda:0" if torch.cuda.is_available() else "cpu",
     )
-    index_collection(dataset_name, model_name, q_encoder, d_encoder, max_id_length, directory, batch_size=8, dim=768,
+    index_collection(dataset_name, model_name, q_encoder, d_encoder, max_id_length, directory, batch_size=8, dim=dim,
                      mode=Mode.MAXP)
 
 
@@ -19,11 +19,16 @@ def index_snowflake_m_collection(dataset_name, max_id_length, directory):
     index_snowflake_collection(dataset_name, max_id_length, directory, model_name)
 
 
+def index_snowflake_xs_collection(dataset_name, max_id_length, directory):
+    model_name = "snowflake-arctic-embed-xs"
+    index_snowflake_collection(dataset_name, max_id_length, directory, model_name, dim=384)
+
+
 def main():
-    dataset_name = "irds:beir/dbpedia-entity"
-    max_id_length = 200
+    dataset_name = "irds:beir/nfcorpus"
+    max_id_length = 8
     directory = "snowflake"
-    index_snowflake_m_collection(dataset_name, max_id_length, directory)
+    index_snowflake_xs_collection(dataset_name, max_id_length, directory)
 
 
 if __name__ == '__main__':
