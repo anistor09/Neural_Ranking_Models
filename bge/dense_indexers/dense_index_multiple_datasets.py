9e4ab31a04d3439a084da8bbf4f47ac6c59f2299
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-05-27 21:38:18 +0200

    Index bge-small scripts + dense indexers

--- bge/dense_indexers/dense_index_multiple_datasets.py
+++ bge/dense_indexers/dense_index_multiple_datasets.py
@@ -1,19 +1,38 @@
-from bge.dense_indexers.dense_index_one_dataset import index_bge_base_collection
+from bge.dense_indexers.dense_index_one_dataset_2 import index_bge_small_collection, index_bge_base_collection
+from func_timeout import func_timeout
 
-datasets = ["quora", "fever"]
-lengths = [6, 221]
+beir_datasets = ["dbpedia-entity", "fever", "fiqa", "hotpotqa", "nfcorpus", "quora", "scifact"]
+lengths = [200, 221, 6, 8, 8, 6, 9]
 prefix_dataset = "irds:beir/"
 directory = "bge"
+datasets = [prefix_dataset + dataset for dataset in beir_datasets]
+datasets.append("irds:msmarco-passage/trec-dl-2019")
+lengths.append(7)
 
 
 def index_bge_base_collections():
+    for index, dataset_name in enumerate(beir_datasets):
+        try:
+            index_bge_base_collection(prefix_dataset + dataset_name, lengths[index], directory)
+            print(dataset_name + " DONE")
+        except Exception as e:
+            # Handles any other exceptions
+            print(f"An error occurred: {e}")
+
+
+def index_bge_small_collections():
     for index, dataset_name in enumerate(datasets):
-        index_bge_base_collection(prefix_dataset + dataset_name, lengths[index], directory)
+        try:
+            index_bge_small_collection(dataset_name, lengths[index], directory)
+            print(dataset_name + " DONE")
+        except Exception as e:
+            # Handles any other exceptions
+            print(f"An error occurred: {e} at dataset {dataset_name}")
 
 
 def main():
     try:
-        index_bge_base_collections()
+        func_timeout(24 * 3600 - 120, index_bge_small_collections)
     except Exception as e:
         # Handles any other exceptions
         print(f"An error occurred: {e}")
--- snowflake/dense_indexers/dense_index_multiple_datasets.py
+++ snowflake/dense_indexers/dense_index_multiple_datasets.py
@@ -2,29 +2,37 @@ from snowflake.dense_indexers.dense_index_one_dataset import index_snowflake_m_c
 from snowflake.dense_indexers.dense_index_one_dataset import index_snowflake_xs_collection
 from func_timeout import func_timeout
 
-beir_datasets = ["scifact"]
-lengths = [9]
+beir_datasets = ["dbpedia-entity", "fever", "fiqa", "hotpotqa", "nfcorpus", "quora", "scifact"]
+lengths = [200, 221, 6, 8, 8, 6, 9]
 prefix_dataset = "irds:beir/"
-directory = "snowflake"
+directory = "bge"
+datasets = [prefix_dataset + dataset for dataset in beir_datasets]
+datasets.append("irds:msmarco-passage/trec-dl-2019")
+lengths.append(7)
 
 
 def index_snowflake_m_collections():
-    for index, dataset_name in enumerate(beir_datasets):
-        index_snowflake_m_collection(prefix_dataset + dataset_name, lengths[index], directory)
-
+    for index, dataset_name in enumerate(datasets):
+        try:
+            index_snowflake_m_collection(dataset_name, lengths[index], directory)
+            print(dataset_name + " DONE")
+        except Exception as e:
+            # Handles any other exceptions
+            print(f"An error occurred: {e}")
 
 def index_snowflake_xs_collections():
-    datasets = [prefix_dataset + dataset for dataset in beir_datasets]
-    # datasets.append("irds:msmarco-passage/trec-dl-2019")
-    # lengths.append(7)
-
-    for index, dataset_name in enumerate(beir_datasets):
-        index_snowflake_xs_collection(prefix_dataset + dataset_name, lengths[index], directory)
+    for index, dataset_name in enumerate(datasets):
+        try:
+            index_snowflake_xs_collection(dataset_name, lengths[index], directory)
+            print(dataset_name + " DONE")
+        except Exception as e:
+            # Handles any other exceptions
+            print(f"An error occurred: {e}")
 
 
 def main():
     try:
-        func_timeout(1 * 3600 - 120, index_snowflake_xs_collections)
+        func_timeout(24 * 3600 - 120, index_snowflake_xs_collections)
     except Exception as e:
         # Handles any other exceptions
         print(f"An error occurred: {e}")
