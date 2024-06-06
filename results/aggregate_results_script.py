14f280774d08f84318a9b3515c26610f2d5d68c5
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-06-06 13:16:44 +0200

    Format results, keep only last value from each table in order to keep them unique, aggregate both latency and reanking results

--- results/aggregate_results_script.py
+++ results/aggregate_results_script.py
@@ -2,34 +2,89 @@ import os
 import pandas as pd
 
 # Usage
-path_to_root = os.path.abspath(os.getcwd()) + "/../"
-output_file = 'aggreagated_latency_results.csv'
+path_to_root = "/"
+# if run from file location -> path_to_root =  "/../"
+path_to_root = os.path.abspath(os.getcwd()) + path_to_root
 directories = ["bge", "gte_base_en_v1_5", "snowflake", "tct_colbert"]
-res_path = "/results/latency_data.csv"
 
 
-def aggregate_csv_files():
+def splits(name):
+    parts = name.split(" ", 1)
+    x = parts[1], parts[0]
+    return x
+
+
+def name_comparator(names):
+    return [splits(name) for name in names]
+
+
+def aggregate_csv_files(input_files, output_file):
     # Create a list to hold data from each CSV file
     data_frames = []
 
-    file_paths = [path_to_root + dir_path + res_path for dir_path in directories]
+    input_files = "/results/" + input_files
+    output_file = path_to_root + "/results/" + output_file
+
+    file_paths = [path_to_root + dir_path + input_files for dir_path in directories]
     print(file_paths)
     # Loop over all files in the directory
     for file_path in file_paths:
         df = pd.read_csv(file_path)
 
         # Append a blank row at the end of each DataFrame
-        blank_row = pd.DataFrame({col: [''] for col in df.columns})
-        df = pd.concat([df, blank_row], ignore_index=True)
+        # blank_row = pd.DataFrame({col: [''] for col in df.columns})
+        # df = pd.concat([df, blank_row], ignore_index=True)
 
         data_frames.append(df)
 
     # Concatenate all the data frames into one
-    combined_df = pd.concat(data_frames, ignore_index=True)
+    duplicate_dataframe = pd.concat(data_frames, ignore_index=True)
+
+    print("BEFORE REMOVING DUPLICATES")
+    print(duplicate_dataframe)
+
+    duplicate_dataframe = duplicate_dataframe.copy()
+
+    if "ranking_metrics" in input_files:
+        duplicate_dataframe[['dataset', 'pipeline_name']] = duplicate_dataframe['name'].str.split(' ', n=1, expand=True)
+        duplicate_dataframe['dataset'] = duplicate_dataframe['dataset'].str[:-1]
+        duplicate_dataframe['model'] = duplicate_dataframe['pipeline_name'].apply(lambda x: x.split(' ')[2])
+
+    else:
+        duplicate_dataframe['model'] = duplicate_dataframe['pipeline_name'].apply(lambda x: x.split(' ')[2])
+
+    unique_dataframe = duplicate_dataframe.groupby('name').tail(1)
+
+    sorted_df = unique_dataframe.sort_values(by='name', key=name_comparator)
+
+    sorted_df = sorted_df.round(4)
 
     # Save the combined data frame to a new CSV file, write header only once
-    combined_df.to_csv(output_file, index=False)
+    sorted_df.to_csv(output_file, index=False)
+
+
+def remove_duplicated_results(input_files):
+    # Create a list to hold data from each CSV file
+
+    file_paths_ranking_metrics = [path_to_root + dir_path + "/results/" + input_files for dir_path in directories]
+
+    print(file_paths_ranking_metrics)
+    # Loop over all files in the directory
+    for file_path in file_paths_ranking_metrics:
+        duplicate_dataframe = pd.read_csv(file_path)
+
+        print("BEFORE REMOVING DUPLICATES")
+        print(duplicate_dataframe)
+        duplicate_dataframe['name'] = duplicate_dataframe['dataset'] + ": " + duplicate_dataframe['pipeline_name']
+        unique_dataframe = duplicate_dataframe.groupby('name').tail(1)
+        unique_dataframe.to_csv(file_path, index=False)
+
+        print("AFTER REMOVING DUPLICATES")
+        print(unique_dataframe)
 
 
 if __name__ == '__main__':
-    aggregate_csv_files()
+    input_files = "latency_data.csv"
+    output_file = "aggreagated_latency_results.csv"
+    aggregate_csv_files(input_files, output_file)
+    # remove_duplicated_results(input_files)
